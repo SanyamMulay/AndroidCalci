71acedf8507bf158befbd10a57b63305ebdd6ee6
sanyam <sanyamsmulay@gmail.com>
2014-12-15 08:11:51 +0530

    UI-basics done.

---
+++ AndroidCalci/src/com/saedutech/androidcalci/MainActivity.java
@@ -0,0 +1,27 @@
+package com.saedutech.androidcalci;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        
+        // Set listeners to all buttons
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
